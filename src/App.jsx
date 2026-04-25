25ac82bd614ecd3a1956d87d1ab06e1e5b9fa512
NikharPanchal <Panchalnikhar4920@gmail.com>
2026-04-25 12:07:03 +0530

    feat: custom progressbar component.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ProgressBar from "./components/ProgressBar";
+
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<section id="center">
+				<ProgressBar />
+			</section>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useMemo, useRef, useState } from "react";
+
+import "./Progressbar.css";
+
+const TARGET_COUNT = 100;
+const MS = 1000;
+
+const ProgressBar = () => {
+	const [duration, setDuration] = useState(5);
+	const [isRunning, setIsRunning] = useState(false);
+	const [width, setWidth] = useState(0);
+
+	const timerRef = useRef(null);
+
+	const intervalSpeed = useMemo(
+		() => (duration * MS) / TARGET_COUNT,
+		[duration],
+	);
+
+	const isDisabled = useMemo(() => isRunning && width !== TARGET_COUNT, []);
+
+	useEffect(() => {
+		if (isRunning) {
+			timerRef.current = setInterval(() => {
+				if (width < TARGET_COUNT) {
+					setWidth((prev) => prev + 1);
+				} else {
+					clearInterval(timerRef.current);
+					return TARGET_COUNT;
+				}
+			}, intervalSpeed);
+		}
+
+		return () => clearInterval(timerRef.current);
+	}, [intervalSpeed, isRunning, width]);
+
+	const handleDurationChange = (event) => {
+		const { value } = event.target;
+		setDuration(value);
+	};
+
+	const handleResetTimer = () => {
+		setWidth(0);
+		setIsRunning(false);
+	};
+
+	const handleStartTimer = () => {
+		setIsRunning(true);
+	};
+
+	return (
+		<>
+			<div className="progressbar-container">
+				<input
+					min={1}
+					disabled={isDisabled}
+					type="number"
+					value={duration}
+					className="input-duration"
+					onChange={handleDurationChange}
+				/>
+				<button
+					onClick={handleStartTimer}
+					disabled={isDisabled}
+					className="cta-button"
+				>
+					Start
+				</button>
+				<button onClick={handleResetTimer} className="cta-button">
+					Reset
+				</button>
+			</div>
+			<div className="progressbar-controls">
+				<div
+					className="progress-fill"
+					style={{
+						width: isRunning ? "100%" : "0%",
+						transition: isRunning ? `width linear ${duration}s` : "none",
+					}}
+				></div>
+				<span className="progressbar-text">{`${width}%`}</span>
+			</div>
+		</>
+	);
+};
+
+export default ProgressBar;
